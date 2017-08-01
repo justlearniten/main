04ae4515b2123e4b830e0d25a70c4994e8e0984b
main <justlearniten@gmail.com>
2017-07-31 20:08:25 -0500

    Clean VS web project

---
+++ mainweb/mainweb/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mainweb.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
